be49309689655949a73a4ab609e75095f7458221
marlones95 <116153313+marlones95@users.noreply.github.com>
2024-08-02 12:34:06 +0200

    Add files via upload

---
+++ fMRI analyses/Main Analyses/D2_Decoding_SVR.m
@@ -0,0 +1,92 @@
+function D2_Decoding_SVR(beta_dir, output_dir, labelnames, dec_type, rad, subj_posterior)
+% This Script specifies what features should be decoded and then executes the Decoding analysis.
+
+% Create output directory if it does not exist yet
+if ~exist(output_dir, 'dir')
+    mkdir(output_dir);
+end
+
+% =========================================================================
+%               Configure input for The Decoding Toolbox
+% =========================================================================
+
+clear cfg
+cfg = decoding_defaults;
+cfg.software = 'SPM12';
+
+% Specify where the results should be saved
+cfg.results.overwrite     = 1;
+cfg.results.dir           = output_dir;
+
+% DATA SCALING
+cfg.scale.method          = 'z';    % standardize the data
+cfg.scale.estimation      = 'all';  % only training, only test data, or all
+
+% SEARCHLIGHT SPECIFICATIONS
+cfg.analysis              = dec_type;   % = searchlight
+cfg.searchlight.unit      = 'voxels';   % Searchlight radius in voxels (not mm) 
+cfg.searchlight.radius    = rad;        % 5 voxel radius
+cfg.searchlight.spherical = 0;          % only useful for mm
+% The amount of information you want to have printed on the screen
+% 0: no, 1: normal, output, 2: all):
+cfg.verbose               = 0;  
+
+% Method and model parameters 
+cfg.decoding.method = 'regression'; % Define SVR as a method
+
+% OUTPUTS SPECIFICATION
+cfg.results.output = {'corr', 'zcorr'}; % zcorr is used for the second level
+
+% DISPLAY:
+cfg.plot_selected_voxels  = 0; % 0: no plotting, 1: every step, 2: every second step ...
+% 100: every hundredth step ...
+
+cfg.plot_design           = 1; % plots the design every time
+
+if dec_type == 'roi'
+    cfg.files.mask = masks;
+else
+    cfg.files.mask = [beta_dir filesep 'mask.nii']; % The mask created with the GLM
+end
+
+% =========================================================================
+%                       Specify and run the decoding
+% =========================================================================
+
+%% Extract subjective frequency differences (SFDs) for all conditions
+% First, get the true frequencies for all conditions
+stim_set = [16 16 20 20 24 24 28 28;...
+            12 20 16 24 20 28 24 32];
+% Then, get the subjective log frequencies of the first stimuli (considering
+% Fechner's law and the time-order effect)
+subj_log_f1            = sort(repmat(unique(subj_posterior.muX(1,:)), 1,2));
+% The second frequencies is just the log of the second frequency
+% (considering Fechner's law)
+subj_log_f2            = log(stim_set(2,:));
+
+% Then calculate the SFDs for all 16 conditions by simple subtraction
+subj_log_diffs = zeros(1,16);
+for i = 1:8
+    subj_log_diffs(i*2-1:i*2)     = [subj_log_f2(i) - subj_log_f1(i), subj_log_f1(i) - subj_log_f2(i)];
+end
+
+% Use these SFDs as labels
+labels = [subj_log_diffs'];
+
+% The following function extracts all beta names and corresponding run
+% numbers from the SPM.mat
+regressor_names = design_from_spm(beta_dir);
+
+% Extract all information for the cfg.files structure
+cfg = decoding_describe_data(cfg,labelnames,labels,regressor_names,beta_dir); 
+
+
+% This creates the leave-one-run-out cross validation design:
+cfg.design = make_design_cv(cfg);
+
+% Display the design in the command window
+display_design(cfg);
+
+
+%% Run decoding
+results = decoding(cfg);
---
+++ fMRI analyses/Main Analyses/Second_Level_Decoding_SVR.m
@@ -0,0 +1,91 @@
+%% Second level analysis performing a one-sample t-test, to statistically test for local brain activation patterns
+clc; clear all;
+
+% =========================================================================
+%                   Specify parameters and analyses
+% =========================================================================
+
+data_dir        = 'D:\vDMTC\data';
+GLM_name        = 'Decoding_SVR';
+radius          = 5;
+analysis_name   = ['Searchlight\' 'SVM_r' num2str(radius) 'vox\192s\' GLM_name];
+
+% 'sub-001',,'sub-007','sub-008',...
+%     'sub-009','sub-010','sub-011','sub-012','sub-013','sub-014','sub-015','sub-016',...
+%     'sub-017','sub-018','sub-019','sub-020','sub-021','sub-022','sub-023','sub-024',...
+%     'sub-026','sub-027','sub-028','sub-029','sub-030','sub-031','sub-032','sub-033','sub-034','sub-035',...
+%     'sub-036','sub-037'
+% sub-025 excluded because of problems with projector
+SJs     = {'sub-002','sub-003','sub-004','sub-005','sub-006'};
+
+group_mask      = '36Subjects_SVR'; % mean MNI-space mask for the second level Analysis from the first level masks
+
+% Output directory:
+out_dir         = fullfile(data_dir, 'RFX\TTest', [analysis_name]);
+if ~exist(out_dir, 'dir')
+    mkdir(out_dir);
+end
+cd(out_dir)
+
+% =========================================================================
+%                   Select the decoding files
+% =========================================================================
+
+fs_all=[];
+for sj = 1:length(SJs)
+
+    % select the smoothed, normalized images
+    filt    = ['^s3w'];
+    % Source directory for the 'mean'-images
+    sj_dir =  fullfile(data_dir, SJs{sj}, 'dec_SVR');
+    % select the files
+    f = spm_select('FPList', sj_dir, filt);
+    numVols = size(f,1);
+    % create SPM style file list for model specification
+    fs_all = [fs_all; cellstr(strcat(f, ',1'))];
+end
+
+% =========================================================================
+%                       Setting up the matlabbatch
+% =========================================================================
+
+% create SPM style file list for model specification
+matlabbatch{1}.spm.stats.factorial_design.dir = {out_dir};
+matlabbatch{1}.spm.stats.factorial_design.des.t1.scans = cellstr(fs_all);
+matlabbatch{1}.spm.stats.factorial_design.cov = struct('c', {}, 'cname', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.masking.tm.tm_none = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.im = 0;
+matlabbatch{1}.spm.stats.factorial_design.masking.em = ...
+    cellstr(fullfile(data_dir, 'MNIMask', ['GroupMask_' group_mask '.nii']));
+matlabbatch{1}.spm.stats.factorial_design.globalc.g_omit = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.glonorm = 1;
+
+spm_jobman('run',matlabbatch);
+clear matlabbatch
+
+
+% =========================================================================
+%               Model estimation and contrast specification
+% =========================================================================
+
+%% Model estimation
+matlabbatch{1}.spm.stats.fmri_est.spmmat = {[pwd filesep 'SPM.mat']}; % pwd = current location
+matlabbatch{1}.spm.stats.fmri_est.method.Classical = 1;
+
+spm_jobman('run',matlabbatch);
+clear matlabbatch;
+
+%% Contrast specification
+matlabbatch{1}.spm.stats.con.spmmat(1)                  = {[pwd filesep 'SPM.mat']};
+matlabbatch{1}.spm.stats.con.consess{1}.tcon.name       = ['pos'];
+matlabbatch{1}.spm.stats.con.consess{1}.tcon.convec     = 1;
+matlabbatch{1}.spm.stats.con.consess{1}.tcon.sessrep    = 'none';
+matlabbatch{1}.spm.stats.con.consess{2}.tcon.name       = ['neg'];
+matlabbatch{1}.spm.stats.con.consess{2}.tcon.convec     = -1;
+matlabbatch{1}.spm.stats.con.consess{2}.tcon.sessrep    = 'none';
+matlabbatch{1}.spm.stats.con.delete                     = 0;
+
+spm_jobman('run',matlabbatch);
+clear matlabbatch;
