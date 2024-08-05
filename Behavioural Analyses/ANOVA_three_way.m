2b8c5636af97ce5a2a8aa9e148d894ff9a4deb19
marlones95 <116153313+marlones95@users.noreply.github.com>
2024-08-05 11:19:19 +0200

    Add files via upload

---
+++ Behavioural Analyses/ANOVA_three_way.m
@@ -0,0 +1,103 @@
+%% Three way ANOVA (factors: rule, stimulus order, f1 frequency)
+
+clear
+close all
+clc
+
+%%% Script adapted from Yuan Hao Wu %%%
+
+% Required script: RMAOV33.m 
+% https://de.mathworks.com/matlabcentral/fileexchange/9638-rmaov33
+
+% =========================================================================
+%                       Get subject's performances
+% =========================================================================
+
+% Initialize subject cell array
+SJs = {'01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15',...
+    '16', '17', '18', '19', '20','21', '22', '23', '24', '26', '27', '28', '29', '30', '31', '32',...
+    '33', '34', '35', '36', '37'}; % 25 excluded because of problems with projector
+sjs = size(SJs,2); % number of subjects
+
+% Directory containing log files
+data_dir = 'D:\vDMTC\Logs';
+% Initialize performance vector 
+perf_FAS_SAF = [];
+
+% Loop over all subjects to get performance per condition
+for i = 1:sjs
+    sj = SJs{i};
+    logfile = spm_select('FPList', data_dir, ['^fMRI_', sj, '.*\.mat$']);
+    load(logfile);
+
+    FAS_16_12 = mylog.perform(mylog.flutter(:,:,1) == 16 & mylog.flutter(:,:,2) == 12 & mylog.focus == 1);
+    FAS_16_20 = mylog.perform(mylog.flutter(:,:,1) == 16 & mylog.flutter(:,:,2) == 20 & mylog.focus == 1);
+    FAS_20_16 = mylog.perform(mylog.flutter(:,:,1) == 20 & mylog.flutter(:,:,2) == 16 & mylog.focus == 1);
+    FAS_20_24 = mylog.perform(mylog.flutter(:,:,1) == 20 & mylog.flutter(:,:,2) == 24 & mylog.focus == 1);
+    FAS_24_20 = mylog.perform(mylog.flutter(:,:,1) == 24 & mylog.flutter(:,:,2) == 20 & mylog.focus == 1);
+    FAS_24_28 = mylog.perform(mylog.flutter(:,:,1) == 24 & mylog.flutter(:,:,2) == 28 & mylog.focus == 1);
+    FAS_28_24 = mylog.perform(mylog.flutter(:,:,1) == 28 & mylog.flutter(:,:,2) == 24 & mylog.focus == 1);
+    FAS_28_32 = mylog.perform(mylog.flutter(:,:,1) == 28 & mylog.flutter(:,:,2) == 32 & mylog.focus == 1);
+
+    perf_FAS_16_12 = sum(FAS_16_12)/length(FAS_16_12);
+    perf_FAS_16_20 = sum(FAS_16_20)/length(FAS_16_20);
+    perf_FAS_20_16 = sum(FAS_20_16)/length(FAS_20_16);
+    perf_FAS_20_24 = sum(FAS_20_24)/length(FAS_20_24);
+    perf_FAS_24_20 = sum(FAS_24_20)/length(FAS_24_20);
+    perf_FAS_24_28 = sum(FAS_24_28)/length(FAS_24_28);
+    perf_FAS_28_24 = sum(FAS_28_24)/length(FAS_28_24);
+    perf_FAS_28_32 = sum(FAS_28_32)/length(FAS_28_32);
+
+    SAF_16_12 = mylog.perform(mylog.flutter(:,:,1) == 16 & mylog.flutter(:,:,2) == 12 & mylog.focus == 2);
+    SAF_16_20 = mylog.perform(mylog.flutter(:,:,1) == 16 & mylog.flutter(:,:,2) == 20 & mylog.focus == 2);
+    SAF_20_16 = mylog.perform(mylog.flutter(:,:,1) == 20 & mylog.flutter(:,:,2) == 16 & mylog.focus == 2);
+    SAF_20_24 = mylog.perform(mylog.flutter(:,:,1) == 20 & mylog.flutter(:,:,2) == 24 & mylog.focus == 2);
+    SAF_24_20 = mylog.perform(mylog.flutter(:,:,1) == 24 & mylog.flutter(:,:,2) == 20 & mylog.focus == 2);
+    SAF_24_28 = mylog.perform(mylog.flutter(:,:,1) == 24 & mylog.flutter(:,:,2) == 28 & mylog.focus == 2);
+    SAF_28_24 = mylog.perform(mylog.flutter(:,:,1) == 28 & mylog.flutter(:,:,2) == 24 & mylog.focus == 2);
+    SAF_28_32 = mylog.perform(mylog.flutter(:,:,1) == 28 & mylog.flutter(:,:,2) == 32 & mylog.focus == 2);
+
+    perf_SAF_16_12 = sum(SAF_16_12)/length(SAF_16_12);
+    perf_SAF_16_20 = sum(SAF_16_20)/length(SAF_16_20);
+    perf_SAF_20_16 = sum(SAF_20_16)/length(SAF_20_16);
+    perf_SAF_20_24 = sum(SAF_20_24)/length(SAF_20_24);
+    perf_SAF_24_20 = sum(SAF_24_20)/length(SAF_24_20);
+    perf_SAF_24_28 = sum(SAF_24_28)/length(SAF_24_28);
+    perf_SAF_28_24 = sum(SAF_28_24)/length(SAF_28_24);
+    perf_SAF_28_32 = sum(SAF_28_32)/length(SAF_28_32);
+
+    perf_FAS_SAF =  [perf_FAS_SAF;[perf_FAS_16_12, perf_FAS_16_20, perf_FAS_20_16, perf_FAS_20_24, ...
+        perf_FAS_24_20, perf_FAS_24_28, perf_FAS_28_24, perf_FAS_28_32, perf_SAF_16_12,...
+        perf_SAF_16_20, perf_SAF_20_16, perf_SAF_20_24, perf_SAF_24_20, perf_SAF_24_28,...
+        perf_SAF_28_24, perf_SAF_28_32]'];
+end
+
+% =========================================================================
+%                   Initialize and run the ANOVA
+% =========================================================================
+
+% Dependent Variable: Performance per condition
+DV = perf_FAS_SAF;
+
+% Subjects:
+S = kron(1:sjs, ones(1,16))';
+
+%% within-subject factors:
+% Factor 1: task rule (compare f1 against f2 (FAS) vs. f2 against f1 (SAF))
+rule = repmat(kron(1:2, ones(1,8))', sjs, 1);
+
+%Factor 2: stimulus order (f1 > f2 vs. f1 < f2)
+stim_order = repmat(repmat([1:2]', 8,1), sjs, 1); 
+
+% Factor 3: f1 frequency (16 Hz, 20 Hz, 24 Hz, and 28 Hz)
+f1_freq  = repmat(repmat(kron(1:4, ones(1,2))', 2,1), sjs, 1);
+
+
+
+% X - n*5 data matrix (Column 1 = dependent variable (performance; column 2 = rule;
+% column 3 = stimulus order; column 4 = f1 frequency; column 5 = subject;
+X = [DV, rule, stim_order, f1_freq, S];
+clearvars -except X
+
+% Run the ANOVA with alpha = 0.05
+RMAOV33(X, 0.05)
---
+++ Behavioural Analyses/Chi_Squared_tests.m
@@ -0,0 +1,51 @@
+%% Chi Squared Tests:
+% This script performs Pearson chi-square tests comparing the proportions of the two
+% conditions "higher" vs "lower" between left and right motor responses
+clear
+
+% Required script: prop_test.m
+% https://de.mathworks.com/matlabcentral/fileexchange/45966-compare-two-proportions-chi-square
+
+% Initialize Subject cell array
+SJs     = {'sub-001','sub-002','sub-003','sub-004','sub-006','sub-005','sub-007','sub-008',...
+    'sub-009','sub-010','sub-011','sub-012','sub-013', 'sub-014','sub-015','sub-016',...
+    'sub-017','sub-018','sub-019','sub-020','sub-021','sub-022','sub-023','sub-024',...
+    'sub-026','sub-027','sub-029','sub-028','sub-030','sub-031','sub-032','sub-033',...
+    'sub-034','sub-035','sub-036','sub-037'}; % 25 excluded because of problems with projector
+
+% Directory containing log files
+log_dir = 'D:\vDMTC\Logs';
+
+% Initialize relevant vectors
+h_motor = zeros(length(SJs),1); % hypothesis vector (0 = distributions not significantly different,
+% 1 = distributions significantly different)
+p_motor = zeros(length(SJs),1); % vector containing the p-value of the chi-squared test
+chi2stat_motor = zeros(length(SJs),1); % vector containing the chi-squared statistic
+
+for sj = 1:length(SJs)
+    current_sj = SJs{sj}(end-1:end);
+    logfile = spm_select('FPList', log_dir, ['^fMRI_', current_sj, '.*\.mat$']);
+    load(logfile);
+
+    decision_high_left = (mylog.choice_direction == 1 & ((mylog.focus == 1 & mylog.flutter(:,:,1) >...
+        mylog.flutter(:,:,2)) | (mylog.focus == 2 & mylog.flutter(:,:,1) < mylog.flutter(:,:,2))));
+    decision_high_right = (mylog.choice_direction == 2 & ((mylog.focus == 1 & mylog.flutter(:,:,1) >...
+        mylog.flutter(:,:,2)) | (mylog.focus == 2 & mylog.flutter(:,:,1) < mylog.flutter(:,:,2))));
+    decision_low_left = (mylog.choice_direction == 1 & ((mylog.focus == 2 & mylog.flutter(:,:,1) >...
+        mylog.flutter(:,:,2)) | (mylog.focus == 1 & mylog.flutter(:,:,1) < mylog.flutter(:,:,2))));
+    decision_low_right = (mylog.choice_direction == 2 & ((mylog.focus == 2 & mylog.flutter(:,:,1) >...
+        mylog.flutter(:,:,2)) | (mylog.focus == 1 & mylog.flutter(:,:,1) < mylog.flutter(:,:,2)))); 
+
+    X_left = [sum(sum(decision_high_left)),sum(sum(decision_low_left))];
+    X_right = [sum(sum(decision_high_right)),sum(sum(decision_low_right))]; 
+    N = [X_left(1)+X_right(1), X_left(2)+X_right(2)];
+
+    % Chi Squared Tests
+    [h_motor(sj),p_motor(sj), chi2stat_motor(sj)] = prop_test(X_left,N,'true');
+
+end
+
+% Creating a table containing all outputs of the chi-squared tests per
+% participant
+T_motor = table(SJs',h_motor,p_motor,chi2stat_motor);
+disp(T_motor)
